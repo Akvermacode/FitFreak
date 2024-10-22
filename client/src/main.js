d0c6aecc73e9d35ddeca154cbfa0d8ac70b00426
Abhishek Rajput <78433992+abhishekrajput-web@users.noreply.github.com>
2024-10-22 19:00:21 +0530

    Update and rename index.js to main.js

--- client/src/main.js
+++ client/src/main.js
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {AuthProvider} from "./context/auth.jsx";
+import {AuthProvider} from "./context/auth";
 import { CartProvider } from './context/cart.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <AuthProvider>
       <CartProvider>
-    <App />
+    <App/>
       </CartProvider>
     </AuthProvider>
 )
