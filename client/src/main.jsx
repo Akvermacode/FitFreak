c83dfbc8f858714b1315f8c576e78a5bec1b60b9
Abhishek Rajput <78433992+abhishekrajput-web@users.noreply.github.com>
2024-10-22 18:42:08 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {AuthProvider} from "./context/auth";
+import {AuthProvider} from "./context/auth.jsx";
 import { CartProvider } from './context/cart.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
