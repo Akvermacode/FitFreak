753d8c269ed49e1624a3f7dc48749be6c955f7fd
Abhishek Rajput <abhishekrajputcr7@gmail.com>
2024-10-22 18:01:08 +0530

    add frontend and backned folder

---
+++ client/src/utils/fetchData.jsx
@@ -0,0 +1,31 @@
+
+
+const exerciseOptions = {
+	method: 'GET',
+	headers: {
+		
+		    'X-RapidAPI-Key': EXERCISE_API_KEY,
+		'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+	}
+};
+
+const youtubeExerciseOptions = {
+	method: 'GET',
+	headers: {
+	
+		    'X-RapidAPI-Key': YOUTUBE_EXERCEISE_API_KEY
+		'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+	  }
+};
+
+const fetchData  = async (url, options) => {
+    const response = await fetch(url,options);
+    const data = await response.json();
+    // console.log(data);
+    return data;
+}
+
+const BASE_URL="http://localhost:5000";
+
+export {fetchData,exerciseOptions,youtubeExerciseOptions,BASE_URL}
+
